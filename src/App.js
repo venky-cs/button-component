5bdd03240add21aa638428582669978d08f5c444
venkatesh <75250713+venky-cs@users.noreply.github.com>
2021-01-03 14:45:28 +0530

    'create-layout'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,122 @@
 import React from 'react';
+import './App.css'
+import styled from 'styled-components'
+import Button from './Button';
+
+const Heading=styled.h3`
+margin:100px;
+width:93px;
+height:36px;
+font-family:poppins;
+font-size:24px;
+line-height:36px;
+`;
+
+const Row=styled.div`
+margin-left:10%;
+`;
+
+const Flex=styled.div`
+display:flex;
+`
+const RightSpace = styled.div`
+margin-right:50px;`
+
+const Title=styled.p`
+font-family:Ubuntu Mono;
+font-weight:400;
+font-size:12px;
+line-height:12px;
+`;
 
 function App() {
   return (
     <div className="App">
-      
+      <Heading>Buttons</Heading>
+      <Row>
+          <Title>{'<Button />'}</Title>
+           <Button variant="default">Default</Button>
+      </Row>
+      <br/>
+
+      <Row>
+          <Title>{'<Button variant=outline />'}</Title>
+          <Button variant="outline">Default</Button>
+      </Row>
+      <br/>
+
+      <Row>
+          <Title>{'<Button variant=text />'}</Title>
+          <Button variant='text'>Default</Button>
+      </Row>
+      <br/>
+
+      <Row>
+        <Title>{'<Button disableshadow />'}</Title>
+        <Button variant="disableshadow">Default</Button>
+      </Row>
+      <br/>
+
+      <Row>
+        <Title>{'<Button disabled />'}</Title>
+        <Button variant="disabled">Default</Button>
+      </Row>
+      <br/>
+
+      <Row>
+        <Flex>
+          <RightSpace>
+            <Title>{'<Button startIcon=local_grocery_store />'}</Title>
+            <Button icon="start">Default</Button>
+          </RightSpace>
+          <RightSpace>
+            <Title>{'<Button endIcon=local_grocery_store />'}</Title>
+            <Button icon="end">Default</Button>
+          </RightSpace>
+        </Flex>
+      </Row>
+      <br/>
+
+      <Row>
+        <Flex>
+          <RightSpace>
+            <Title>{'<Button size=sm />'}</Title>
+            <Button size="sm">Default</Button>
+          </RightSpace>
+          <RightSpace>
+            <Title>{'<Button size=md />'}</Title>
+            <Button size="md">Default</Button>
+          </RightSpace>
+          <RightSpace>
+            <Title>{'<Button size=lg />'}</Title>
+            <Button size="lg">Default</Button>
+          </RightSpace>
+        </Flex>
+      </Row>
+      <br/>
+
+      <Row>
+        <Flex>
+          <RightSpace>
+            <Title>{'<Button color=default />'}</Title>
+            <Button color='default'>Default</Button>
+          </RightSpace>
+          <RightSpace>
+            <Title>{'<Button color=primary />'}</Title>
+            <Button color='primary'>Default</Button>
+          </RightSpace>
+          <RightSpace>
+            <Title>{'<Button color=secondary />'}</Title>
+            <Button color='secondary'>Default</Button>
+          </RightSpace>
+          <RightSpace>
+            <Title>{'<Button color=danger />'}</Title>
+            <Button color='danger'>Default</Button>
+          </RightSpace>
+        </Flex>
+      </Row>
+      <br/>
+
     </div>
   );
 }
---
+++ src/Button.js
@@ -0,0 +1,105 @@
+import styled from 'styled-components';
+
+const Button =styled.button`
+width:150px;
+height:50px;
+margin:10px 0;
+padding:8px 20px;
+border-radius:12px;
+border:none;
+cursor:pointer;
+
+
+${props => {
+    if(props.variant ==='default'){
+        return `border:none;
+        background-color: rgba(204, 198, 198, 0.568);
+
+        &:hover {
+        background-color:grey}`
+    }
+    if(props.variant === 'outline'){
+        return `border:1px solid blue;
+        color:blue;
+        background-color:white;
+        
+        &:hover {border:1px solid blue;
+        color:rgba(17, 17, 158, 0.877);
+        background-color: rgba(138, 138, 216, 0.788)}`;
+    }
+    if(props.variant === 'text'){
+        return `border:none;
+        color:rgba(17, 17, 158, 0.877);
+        background-color:white;
+        
+        &:hover {border:none;
+        color:rgba(17, 17, 158, 0.877);
+        background-color:rgba(130, 130, 200, 0.877)}`;
+    }
+    if (props.variant === 'disableshadow'){
+        return `
+        background-color:rgba(17, 17, 158, 0.877);
+        color:#fff`
+    }
+    if (props.variant === 'disabled'){
+        return `
+        cursor:default;
+        border:none;
+        background-color: rgba(204, 198, 198, 0.568);
+        color:rgba(39, 38, 38, 0.507);
+
+        &:hover {
+            color: rgba(224, 198, 198, 0.568);
+            background-color:#fff;
+        }
+        `
+    }
+}}
+
+
+${props => {
+    switch(props.size){
+        case "sm":
+            return 'font-size:12px;width:100px;background-color:rgba(17, 17, 158, 0.877);color:white;';
+
+        case "md":
+            return 'font-size:16px;width:130px;background-color:rgba(17, 17, 158, 0.877);color:white;';
+
+        case "lg":
+            return 'font-size:20px;background-color:rgba(17, 17, 158, 0.877);color:white;';
+    }
+    return null
+}}
+
+
+${props => {
+    switch(props.color){
+        case 'default':
+            return `
+             background-color: rgba(204, 198, 198, 0.568);
+        &:hover {
+        background-color:grey}
+            `;
+        case 'primary':
+            return `color:white;background-color:rgba(17, 17, 158, 0.877);
+            &:hover{
+                background-color:blue;
+            }`;
+        case 'secondary':
+            return `color:white;background-color:rgba(0, 0, 0, 0.521);
+            &:hover{
+                background-color:black;
+            }`;
+        case 'danger':
+            return `color:white;background-color:rgba(255, 0, 0, 0.459);
+            &:hover{
+                background-color:red;
+            }`;
+    }
+    return null;
+}}
+`;
+
+
+
+export default Button
